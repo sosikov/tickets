99c3330ad9030457909554ba9135b5cba0f78470
sosikov <n.sosikov@gmail.com>
2020-03-14 19:21:11 +0300

    added Tickets component

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+import Tickets from '../Tickets/Tickets';
+
+function App() {
+  return (
+    <div>
+      <Tickets/>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Tickets/Tickets.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import './Tickets.scss';
+
+const Tickets = () => {
+  return (
+    <div className='tickets'>
+
+      <div className='tickets__item'>
+        <table className='tickets__table'>
+          <thead>
+            <tr>
+              <td colSpan={2} className='tickets__price'>13 400 P</td>
+              <td><img className='tickets__logo' src='//pics.avs.io/99/36/TG.png' alt='TG'/></td>
+            </tr>
+          </thead>
+          <tbody className='table__body'>
+            <tr className='tickets__row'>
+              <td className='tickets__cell'>
+                <span className='tickets__label'>MOW – HKT</span>
+                <span className='tickets__text'>10:45 – 08:00</span>
+              </td>
+              <td className='tickets__cell'>
+                <span className='tickets__label'>В пути</span>
+                <span className='tickets__text'>21ч 15м</span>
+              </td>
+              <td className='tickets__cell'>
+                <span className='tickets__label'>2 пересадки</span>
+                <span className='tickets__text'>HKG, JNB</span>
+              </td>
+            </tr>
+          </tbody>
+          <tbody className='table__body'>
+            <tr className='tickets__row'>
+              <td className='tickets__cell'>
+                <span className='tickets__label'>MOW – HKT</span>
+                <span className='tickets__text'>10:45 – 08:00</span>
+              </td>
+              <td className='tickets__cell'>
+                <span className='tickets__label'>В пути</span>
+                <span className='tickets__text'>21ч 15м</span>
+              </td>
+              <td className='tickets__cell'>
+                <span className='tickets__label'>2 пересадки</span>
+                <span className='tickets__text'>HKG, JNB</span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+    </div>
+  )
+}
+
+export default Tickets;
