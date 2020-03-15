585803a8c00d64a18febc3766f4d781ca6d7cf71
sosikov <n.sosikov@gmail.com>
2020-03-15 12:56:19 +0300

    added logo component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 import Tickets from '../Tickets/Tickets';
 import Tabs from '../Tabs/Tabs';
 import Filters from '../Filters/Filters';
+import Logo from '../Logo/Logo';
 import './App.scss';
 
 const App = () => {
   return (
-    <div className='app__container'>
-      <div className='app__filters'>
-        <Filters/>
-      </div>
-      <div className='app__tickets'>
-        <Tabs/>
-        <Tickets/>
+    <div className='app_root'>
+      <Logo/>
+      <div className='app__container'>
+        <div className='app__filters'>
+          <Filters/>
+        </div>
+        <div className='app__tickets'>
+          <Tabs/>
+          <Tickets/>
+        </div>
       </div>
     </div>
   )
